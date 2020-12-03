1456ad228b54827b91ee45797111ffcf77dff491
blagowestas <blagovesta.simonovaa@gmail.com>
2020-12-03 11:04:51 +0200

    Fix tree? function in tree.rkt.
    Add skip-whitespaces function and change the recursion in left and right tree.

--- tree.rkt
+++ tree.rkt
@@ -59,60 +59,64 @@
 
   (helper (+ 1 first-index) 0) ; прескачаме първата скоба, не искаме да я броим
 ) 
-  
-  
+
+(define (skip-whitespaces str index) ;прескача интервалите
+  (if (not (char-whitespace? (string-ref str index))) index
+      (skip-whitespaces str (+ 1 index)))
+)
 
 
-
-(define (tree? str)
- 
-  (define (root? str index)
+(define (root? str index)
     (is-number? (string-ref str index))
     )
 
-  (define (is-tree? str first-index last-index) ; ПОМИСЛИ КАК ДА ПРЕСКАЧАШ СПЕЙСОВЕТЕ
+  (define (is-tree? str first-index last-index) ;
     (if (and (char=? (string-ref str first-index) #\*) (= first-index last-index)) #t
         (and
          (char=? (string-ref str first-index) #\{)
-         (root? str (+ 1 first-index))
          (= (number-of-children str first-index last-index) 2)
-         (let* (
-                (first-index-left-tree (get-first-index-after-number str (+ 1 first-index))) 
-                (last-index-left-tree (if (char=? (string-ref str first-index-left-tree) #\*) first-index-left-tree (get-last-index str first-index-left-tree 0)))          
-                (first-index-right-tree (if (false? last-index-left-tree) #f (+ 1 last-index-left-tree)))                         
-                (last-index-right-tree (cond
-                                         ((false? first-index-right-tree) #f)
-                                         ((char=? (string-ref str first-index-right-tree) #\*) first-index-right-tree)
-                                         (else (get-last-index str first-index-right-tree 0))))        
+
+         (let* ( ;тук проверяваме дали имаме валиден корен и ляво поддърво
+                (root-index (skip-whitespaces str (+ 1 first-index)))
+                
+                (first-index-left-tree (skip-whitespaces str (get-first-index-after-number str root-index)))
+                
+                (last-index-left-tree (if (empty-string-tree? str first-index-left-tree)
+                                          first-index-left-tree
+                                          (get-last-index str first-index-left-tree 0)))
+                
                 )
-           (if (or (false? first-index-left-tree) (false? last-index-left-tree) (false? first-index-right-tree) (false? last-index-left-tree))
-               #f
-               (and (is-tree? str first-index-left-tree last-index-left-tree)
-                    (is-tree? str first-index-right-tree last-index-right-tree)
-                    )
-               )
+           (and
+            (root? str root-index)
+            (is-tree? str first-index-left-tree last-index-left-tree) ;ако имаме валидно ляво поддърво, следващият индекс е на дясното, трябва да го проверим
+
+            (let* (
+                   (first-index-right-tree (skip-whitespaces str (+ 1 last-index-left-tree)))
+                   (last-index-right-tree (if (empty-string-tree? str first-index-right-tree)
+                                              first-index-right-tree
+                                              (get-last-index str first-index-right-tree 0)))
+                   )
+              (is-tree? str first-index-right-tree last-index-right-tree)
+            )
            )
-         (char=? (string-ref str last-index) #\}))
+         )
+        (char=? (string-ref str last-index) #\}))
         )
     )
 
 
-  (if (correct-brackets? str 0 0)
-      (let* (
-             (str-without-whitespace (remove-whitespace str 0))
-             (len (string-length str-without-whitespace))
-             )        
-        (is-tree? str-without-whitespace 0 (- len 1))
-        )
-      #f)
+(define (tree? str)
+  (if (not (correct-brackets? str 0 0)) #f
+      (is-tree? str 0 (- (string-length str) 1))
   )
+)
 
-
-(trace tree?)
+(trace skip-whitespaces)
+(trace is-tree?)
 (trace correct-brackets?)
 ;(trace root?)
-;(trace get-first-index-after-number)
-;(trace get-last-index)
+(trace get-first-index-after-number)
+(trace get-last-index)
 (trace number-of-children)
 
 (define (string->tree str)
@@ -234,3 +238,63 @@
 ;(balanced? '(a (b (d (f (y () ()) ()) ()) (g () ())) (c (e (g () ()) ()) (f () ())))) - f
 ;(balanced? '(a (b (d (f (y () ()) ()) (h () ())) (g (d () ()) ())) (c (e (g () ()) ()) (f () ())))) - t
 ;(balanced? '(a (b (d (f (y () ()) ()) (h () ())) (g (d () ()) ())) (c (e () ()) (f () ())))) - f
+
+
+
+
+
+
+(test/gui
+ (test-suite
+    "all tests"
+    (test-suite
+     "Testing tree?"
+     (test-case "Empty tree" (check-true (tree? "*")))
+     (test-case "Tree with empty children trees and a lot whitespacces" (check-true (tree? "{  10   *   *}")))
+     (test-case "Correct tree with one child" (check-true (tree? "{10 { 5 * * } *}")))
+     (test-case "Tree with two children and no whitespaces" (check-true (tree? "{10{5**}{15**}}")))
+     (test-case "Complex tree" (check-true (tree? "{5 {22 {2 * *} {6 * *}} {1 * {3 {111 * *} *}}}")))
+     (test-case "Tree with more than 2 children" (check-false (tree? "{5 {22 {2 * *} {6 * *}} {1 * {3 {111 * *} *}} {1 * *}}")))
+     (test-case "Tree with only one child." (check-false (tree? "{5 {22 {2 * *} *}}")))
+     (test-case "Tree with more roots" (check-false (tree? "{1 2 3 * * }")))
+     (test-case "Wrong tree 1" (check-false (tree? "{}")))
+     (test-case "Wrong tree 2" (check-false (tree? "{*}")))
+     (test-case "Wrong tree 3" (check-false (tree? "{10{*{{}10")))
+     (test-case "Wrong tree 4" (check-false (tree? "{10*10}")))
+     (test-case "Wrong tree 5" (check-false (tree? "}{")))
+     (test-case "Wrong tree 6" (check-false (tree? "***")))
+    )
+    (test-suite
+     "Testing ordered?"
+     (test-case "Empty tree" (check-true (ordered? '())))
+     (test-case "Simple tree" (check-true (ordered? '(10 () ()))))
+     (test-case "Complex tree" (check-true (ordered? '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))))))
+     (test-case "Not-ordered tree 1" (check-false (ordered? '(15 (25 (0 () ()) (10 () ())) ()))))
+     (test-case "Not-ordered tree 2" (check-false (ordered? '(15 (5 (0 () ()) (10 () ())) (10 () ())))))
+     (test-case "Not-ordered tree 3" (check-false (ordered? '(15 (25 () ()) (10 () ())))))
+    )
+     (test-suite
+     "Testing balanced?"
+     (test-case "Empty tree" (check-true (balanced? '())))
+     (test-case "Simple tree" (check-true (balanced? '(10 () ()))))
+     (test-case "Complex tree 1" (check-true (balanced? '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))))))
+     (test-case "Complex tree 2" (check-true (balanced? '(1 (2 (3 () ()) ()) (4 (5 (6 () ()) ()) (7 () ()))))))
+     (test-case "Complex tree 3" (check-true (balanced? '(4 (6 (7 (8 (9 () ()) ()) (10 () ())) (11 (12 () ()) ())) (13 (14 (15 () ()) ()) (16 () ()))))))
+     (test-case "Not-ordered tree 1" (check-false (balanced? '(1 (2 (3 () ()) ()) (4 (5 (6 (7 () ()) ()) ()) (8 () ()))))))
+     (test-case "Not-ordered tree 2" (check-false (balanced? '(1 (2 (3 (4 (5 () ()) ()) ()) (6 () ())) (7 (8 (9 () ()) ()) (10 () ()))))))
+     (test-case "Not-ordered tree 3" (check-false (balanced? '(1 (2 (3 (4 (5 () ()) ()) (6 () ())) (7 (8 () ()) ())) (9 (10 () ()) (11 () ()))))))
+    )
+     (test-suite
+     "Testing tree->stream?"
+     (test-case "Empty tree, inorder" (check-equal? (tree->stream empty-tree "inorder") empty-stream))
+     (test-case "Empty tree, postorder" (check-equal? (tree->stream empty-tree "postorder") empty-stream))
+     (test-case "Empty tree, preorder" (check-equal? (tree->stream empty-tree "preorder") empty-stream))
+     (test-case "Simple tree, inorder" (check-equal? (stream->list (tree->stream '(10 () ()) "inorder")) '(10)))
+     (test-case "Simple tree, postorder" (check-equal? (stream->list (tree->stream '(10 () ()) "postorder")) '(10)))
+     (test-case "Simple tree, preorder" (check-equal? (stream->list (tree->stream '(10 () ()) "preorder")) '(10)))
+     (test-case "BST, inorder" (check-equal? (stream->list (tree->stream '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))) "inorder")) '(0 5 10 15 20 25 30)))
+     (test-case "BST, postorder" (check-equal? (stream->list (tree->stream '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))) "postorder")) '(0 10 5 20 30 25 15)))
+     (test-case "BST, preorder" (check-equal? (stream->list (tree->stream '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))) "preorder")) '(15 5 0 10 25 20 30)))
+    )
+   )
+)
