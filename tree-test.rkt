501e7af54a44637e201267b5e18d2b8180b42f3e
blagowestas <blagovesta.simonovaa@gmail.com>
2020-12-07 14:03:47 +0200

    Final version

--- tree-test.rkt
+++ tree-test.rkt
@@ -1,27 +1,28 @@
-#lang racket
-
+#lang racket/base
 (require racket/include)
 (require rackunit)
 (require rackunit/gui)
+(require racket/stream)
 
 (require "tree.rkt")
 
 
 (test/gui
  (test-suite
-    "all tests"
+    "All tests"
     (test-suite
      "Testing 'tree?'"
      (test-case "Empty tree" (check-true (tree? "*")))
      (test-case "Empty tree with whitespaces" (check-true (tree? "   *")))
-     (test-case "Tree with empty children trees and a lot whitespacces" (check-true (tree? "   {  10   *   *}  ")))
-     (test-case "Correct tree with one child" (check-true (tree? "{10 { 5 * * } *}")))
-     (test-case "Tree with two children and no whitespaces" (check-true (tree? "{10{5**}{15**}}")))
+     (test-case "Tree with empty subtrees and a lot whitespacces" (check-true (tree? "   {  10   *   *}  ")))
+     (test-case "Correct tree with subtree" (check-true (tree? "{10 { 5 * * } *}")))
+     (test-case "Tree with two subtrees and no whitespaces" (check-true (tree? "{10{5**}{15**}}")))
      (test-case "Complex tree" (check-true (tree? "{5 {22 {2 * *} {6 * *}} {1 * {3 {111 * *} *}}}")))
-     (test-case "Tree with more than 2 children" (check-false (tree? "{5 {22 {2 * *} {6 * *}} {1 * {3 {111 * *} *}} {1 * *}}")))
-     (test-case "Tree with only one child." (check-false (tree? "{5 {22 {2 * *} *}}")))
+     (test-case "Tree with more than 2 subtrees" (check-false (tree? "{5 {22 {2 * *} {6 * *}} {1 * {3 {111 * *} *}} {1 * *}}")))
+     (test-case "Tree with only one subtree." (check-false (tree? "{5 {22 {2 * *} *}}")))
      (test-case "Tree with more roots" (check-false (tree? "{1 2 3 * * }")))
      (test-case "Missing root" (check-false (tree? "{ * {1 * *}}")))
+     (test-case "Empty string" (check-false (tree? "")))
      (test-case "Wrong empty tree" (check-false (tree? "{}")))
      (test-case "Wrong empty tree 2" (check-false (tree? "*}")))
      (test-case "Wrong empty tree 3" (check-false (tree? "* 12")))
@@ -36,9 +37,9 @@
      (test-case "Empty tree" (check-true (ordered? '())))
      (test-case "Simple tree" (check-true (ordered? '(10 () ()))))
      (test-case "Complex tree" (check-true (ordered? '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))))))
-     (test-case "Not-ordered tree 1" (check-false (ordered? '(15 (25 (0 () ()) (10 () ())) ()))))
-     (test-case "Not-ordered tree 2" (check-false (ordered? '(15 (5 (0 () ()) (10 () ())) (10 () ())))))
-     (test-case "Not-ordered tree 3" (check-false (ordered? '(15 (25 () ()) (10 () ())))))
+     (test-case "Unordered tree 1" (check-false (ordered? '(15 (25 (0 () ()) (10 () ())) ()))))
+     (test-case "Unordered tree 2" (check-false (ordered? '(15 (5 (0 () ()) (10 () ())) (10 () ())))))
+     (test-case "Unordered tree 3" (check-false (ordered? '(15 (25 () ()) (10 () ())))))
      (test-case "Invalid tree" (check-equal? (ordered? '(1 (2 (3 () ())) (4 (5 (6 () ()) ()) (7 () ())))) "Invalid tree"))
     )
      (test-suite
@@ -48,9 +49,9 @@
      (test-case "Complex tree 1" (check-true (balanced? '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))))))
      (test-case "Complex tree 2" (check-true (balanced? '(1 (2 (3 () ()) ()) (4 (5 (6 () ()) ()) (7 () ()))))))
      (test-case "Complex tree 3" (check-true (balanced? '(4 (6 (7 (8 (9 () ()) ()) (10 () ())) (11 (12 () ()) ())) (13 (14 (15 () ()) ()) (16 () ()))))))
-     (test-case "Not-ordered tree 1" (check-false (balanced? '(1 (2 (3 () ()) ()) (4 (5 (6 (7 () ()) ()) ()) (8 () ()))))))
-     (test-case "Not-ordered tree 2" (check-false (balanced? '(1 (2 (3 (4 (5 () ()) ()) ()) (6 () ())) (7 (8 (9 () ()) ()) (10 () ()))))))
-     (test-case "Not-ordered tree 3" (check-false (balanced? '(1 (2 (3 (4 (5 () ()) ()) (6 () ())) (7 (8 () ()) ())) (9 (10 () ()) (11 () ()))))))
+     (test-case "Unbalanced tree 1" (check-false (balanced? '(1 (2 (3 () ()) ()) (4 (5 (6 (7 () ()) ()) ()) (8 () ()))))))
+     (test-case "Unbalanced tree 2" (check-false (balanced? '(1 (2 (3 (4 (5 () ()) ()) ()) (6 () ())) (7 (8 (9 () ()) ()) (10 () ()))))))
+     (test-case "Unbalanced tree 3" (check-false (balanced? '(1 (2 (3 (4 (5 () ()) ()) (6 () ())) (7 (8 () ()) ())) (9 (10 () ()) (11 () ()))))))
      (test-case "Invalid tree" (check-equal? (balanced? '(1 (2 (3 () ())) (4 (5 (6 () ()) ()) (7 () ())))) "Invalid tree"))
     )
      (test-suite
@@ -64,7 +65,7 @@
      (test-case "BST, inorder" (check-equal? (stream->list (tree->stream '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))) 'inorder)) '(0 5 10 15 20 25 30)))
      (test-case "BST, postorder" (check-equal? (stream->list (tree->stream '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))) 'postorder)) '(0 10 5 20 30 25 15)))
      (test-case "BST, preorder" (check-equal? (stream->list (tree->stream '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ()))) 'preorder)) '(15 5 0 10 25 20 30)))
-     (test-case "Invalid tree" (check-equal? (tree->stream '(1 (2 (3 () ())) (4 (5 (6 () ()) ()) (7 () ()))) "inorder") "Invalid tree"))
+     (test-case "Invalid tree" (check-equal? (tree->stream '(1 (2 (3 () ())) (4 (5 (6 () ()) ()) (7 () ()))) 'inorder) "Invalid tree"))
     )
      (test-suite
      "Testing 'tree->string?'"
--- tree.rkt
+++ tree.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang racket/base
 (require racket/trace)
 (require racket/stream)
 (provide (all-defined-out))
@@ -13,7 +13,7 @@
 (define (get-first-index-after-number str index) 
   (if (is-number? (string-ref str index)) (get-first-index-after-number str (+ 1 index))
       index)
-)
+  )
   
 (define (get-last-index str index number-of-opened-brackets) ;взима индекса на правилната затваряща скоба
   (cond 
@@ -23,7 +23,7 @@
     ((and (char=? (string-ref str index) #\}) (> number-of-opened-brackets 1)) (get-last-index str (+ 1 index) (- number-of-opened-brackets 1)))
     (else (get-last-index str (+ 1 index) number-of-opened-brackets))
     )
-) 
+  ) 
 
 (define (remove-whitespace str index)
   (cond
@@ -45,7 +45,7 @@
 
 (define (tree? str)
 
-  (define (check-subtrees str index subs)
+  (define (check-subtrees str index subs) ;проверявам децата под формата на стек чрез списък
     (cond
       ((= index (string-length str)) #t)
       ((and (char=? (string-ref str index) #\*) (null? subs))   (or (= (+ 1 index) (string-length str)) (= (skip-whitespaces str (+ 1 index)) (string-length str))));empty tree
@@ -70,29 +70,29 @@
       (else (correct-brackets? str (+ 1 index) counter))))
 
 
-  (define (check-tree str index should-be child) ;
+  (define (check-tree str index should-be subtree) ;
     (cond
       ((= index (string-length str)) #t)
-      ((char-whitespace? (string-ref str index)) (check-tree str (+ 1 index) should-be child))
+      ((char-whitespace? (string-ref str index)) (check-tree str (+ 1 index) should-be subtree))
 
-      ((and (char=? (string-ref str index) #\*)  (equal? should-be 'beg) (equal? child 'main)) ;проверка за празното дърво
+      ((and (char=? (string-ref str index) #\*)  (equal? should-be 'beg) (equal? subtree 'main)) ;проверка за празното дърво
        (or (= (+ 1 index) (string-length str)) (= (skip-whitespaces str (+ 1 index)) (string-length str))))  
       ((and (is-number? (string-ref str index)) (equal? should-be 'number)) (check-tree str (get-first-index-after-number str index) 'beg 'left)) ;beg = { or *
-      ((and (char=? (string-ref str index) #\{) (equal? should-be 'beg)) (check-tree str (+ 1 index) 'number child))
-      ((and (char=? (string-ref str index) #\*) (equal? should-be 'beg) (equal? child 'left)) (check-tree str (+ 1 index) 'beg 'right))
-      ((and (char=? (string-ref str index) #\*) (equal? should-be 'beg) (equal? child 'right)) (check-tree str (+ 1 index) 'end 'right)) 
-      ((and (char=? (string-ref str index) #\}) (equal? should-be 'end) (equal? child 'left)) (check-tree str (+ 1 index) 'beg 'right))
-      ((and (char=? (string-ref str index) #\}) (equal? should-be 'end) (equal? child 'right)) (or (= (skip-whitespaces str index) (string-length str))
+      ((and (char=? (string-ref str index) #\{) (equal? should-be 'beg)) (check-tree str (+ 1 index) 'number subtree))
+      ((and (char=? (string-ref str index) #\*) (equal? should-be 'beg) (equal? subtree 'left)) (check-tree str (+ 1 index) 'beg 'right))
+      ((and (char=? (string-ref str index) #\*) (equal? should-be 'beg) (equal? subtree 'right)) (check-tree str (+ 1 index) 'end 'right)) 
+      ((and (char=? (string-ref str index) #\}) (equal? should-be 'end) (equal? subtree 'left)) (check-tree str (+ 1 index) 'beg 'right))
+      ((and (char=? (string-ref str index) #\}) (equal? should-be 'end) (equal? subtree 'right)) (or (= (skip-whitespaces str index) (string-length str))
                                                                                                    (check-tree str (+ 1 index) 'end 'right)
                                                                                                    (check-tree str (+ 1 index) 'beg 'right)))
       (else #f))
     )
 
   
-  (if (and (correct-brackets? str 0 0) (check-subtrees str 0 '()))
+  (if (and (correct-brackets? str 0 0) (check-subtrees str 0 '()) (not (string=? "" str)) )
       (check-tree str 0 'beg 'main)
       #f)
-)
+  )
     
 
 (define (string->tree str)
@@ -151,7 +151,41 @@
     (= (length tree) 3)
     (is-valid? (left-tree tree))
     (is-valid? (right-tree tree))))
-)
+  )
+
+(define (height tree)
+  (if (empty? tree) 0
+      (+ 1 (max
+            (height (left-tree tree))
+            (height (right-tree tree))))
+      )
+  )
+
+
+(define (width tree)
+
+  (define (level-elements n tree)
+    (cond
+      ((empty? tree) '())
+      ((= n 1) (list (tree-root tree)))
+      (else (append (level-elements (- n 1) (left-tree tree)) (level-elements (- n 1) (right-tree tree)))))
+    )
+
+
+  (define (get-level-list n tree) ;подаваме височината на дървото, която съвпада с броя на нивата
+    (cond
+      ((zero? n) '())
+      (else (append (list (level-elements n tree)) (get-level-list (- n 1) tree))))
+    )
+
+  
+  (foldr
+   (lambda (current result) (max (length current) result))
+   0
+   (get-level-list (height tree) tree))
+
+  )
+
 
 (define (ordered? tree)
   (if (not (is-valid? tree))
@@ -169,15 +203,6 @@
             )
           )
       )
-)
-
-
-(define (height tree)
-  (if (empty? tree) 0
-      (+ 1 (max
-            (height (left-tree tree))
-            (height (right-tree tree))))
-      )
   )
 
 
@@ -192,10 +217,10 @@
             (and (<= (abs (- (height left-tree) (height right-tree))) 1)
                  (balanced? left-tree)
                  (balanced? right-tree))
+            )
           )
-       )
-    )
-)
+      )
+  )
 
 
 (define (tree->string tree)
@@ -210,11 +235,12 @@
            " "
            (tree->string (right-tree tree))
            "}"
+           )
           )
-       )
       )
-)
-                  
+  )
+
+
 (define (tree->stream tree order)
 
   (define (make-stream-inorder tree)
@@ -242,127 +268,61 @@
   )
 
 
-(define (level-elements n tree)
-  (cond
-    ((empty? tree) '())
-    ((= n 1) (list (tree-root tree)))
-    (else (append (level-elements (- n 1) (left-tree tree)) (level-elements (- n 1) (right-tree tree)))))
-)
-
-
-(define (get-level-list n tree) ;подаваме височината на дървото, която съвпада с броя на нивата
-  (cond
-    ((zero? n) '())
-    (else (append (list (level-elements n tree)) (get-level-list (- n 1) tree))))
-)
-
-
-                   
-(define (width tree)
-  (foldr
-   (lambda (current result) (max (length current) result))
-   0
-   (get-level-list (height tree) tree))
-)
-
-
-(define (get-n-chars n ch)
-  (define (helper n result)
-    (if (< n 1) result
-      (helper (- n 1) (string-append ch result))))
-  (helper n ""))
-
-
-(define (get-level tree elem)
-  
-  (define (helper current-level current-tree)
-    (cond
-      ((or (> current-level (height tree)) (empty? current-tree)) #f)
-      ((= (tree-root current-tree) elem) current-level)
-      (else (or (helper (+ 1 current-level) (left-tree current-tree)) (helper (+ 1 current-level) (right-tree current-tree))))))
-
-  (helper 1 tree))
-
-
-
-(define (number-of-digits number)
-  (define (helper current-number counter)
-    (if (zero? current-number) counter
-        (helper (quotient current-number 10) (+ 1 counter))))
-
-     (if (zero? number) 1 (helper number 0)))
-
-(define (print-left tree height-right number-of-spaces) 
-  (cond
-    ((empty? tree) "")
-    ((is-leaf? tree) (string-append "\n" (get-n-chars (- number-of-spaces 1) " ") "|\n" (get-n-chars (- number-of-spaces 1) " ") (number->string (tree-root tree))))
-    (else   (string-append
-       "\n"
-       (get-n-chars number-of-spaces " ")
-       "|\n"
-       (get-n-chars number-of-spaces " ")
-       (number->string (tree-root tree))
-       (print-right (right-tree tree) (width (left-tree tree))  number-of-spaces)
-       (print-left (left-tree tree) (height (right-tree tree))  number-of-spaces))))
-)
-
-  
-(define (print-right tree width-left number-of-spaces)
-  (cond
-    ((empty? tree) "")
-    ((is-leaf? tree) (string-append "--" (number->string (tree-root tree))))
-    (else
-     (string-append 
-      (get-n-chars (+ 1 width-left) "--")
-      (number->string (tree-root tree))
-      (print-right (right-tree tree) (width (left-tree tree)) (+ (* 2 (+ 1 width-left)) number-of-spaces (number-of-digits (tree-root tree))))
-      (print-left (left-tree tree) (height (right-tree tree)) (+ (* 2 (+ 1 width-left)) number-of-spaces (number-of-digits (tree-root tree)))))))
-    )
- 
 
 (define (visualize tree) 
- 
+
+  (define (get-n-chars n ch)
+    (define (helper n result)
+      (if (< n 1) result
+          (helper (- n 1) (string-append ch result))))
+    (helper n ""))
+
+  (define (number-of-digits number)
+    (define (helper current-number counter)
+      (if (zero? current-number) counter
+          (helper (quotient current-number 10) (+ 1 counter))))
+
+    (if (zero? number) 1 (helper number 0)))
+
+  (define (print-left tree height-right number-of-spaces) 
+    (cond
+      ((empty? tree) "")
+      ((is-leaf? tree) (string-append "\n" (get-n-chars (- number-of-spaces 1) " ") "|\n" (get-n-chars (- number-of-spaces 1) " ") (number->string (tree-root tree))))
+      (else   (string-append
+               "\n"
+               (get-n-chars (- number-of-spaces 1) " ")
+               "|\n"
+               (get-n-chars (- number-of-spaces 1) " ")
+               (number->string (tree-root tree))
+               (print-right (right-tree tree) (width (left-tree tree))  number-of-spaces)
+               (print-left (left-tree tree) (height (right-tree tree))  number-of-spaces))))
+    )
+
+  
+  (define (print-right tree width-left number-of-spaces)
+    (cond
+      ((empty? tree) "")
+      ((is-leaf? tree) (string-append "--" (number->string (tree-root tree))))
+      (else
+       (string-append 
+        (get-n-chars (+ 1 width-left) "--")
+        (number->string (tree-root tree))
+        (print-right (right-tree tree) (width (left-tree tree)) (+ (* 2 (+ 1 width-left)) number-of-spaces (number-of-digits (tree-root tree))))
+        (print-left (left-tree tree) (height (right-tree tree)) (+ (* 2 (+ 1 width-left)) number-of-spaces (number-of-digits (tree-root tree)))))))
+    )
+
+
   (cond
     ((not (is-valid? tree)) "Invalid tree")
     ((empty? tree) "")
     (else
-     (string-append
-      (number->string (tree-root tree))
-      (print-right (right-tree tree) (width (left-tree tree)) (number-of-digits (tree-root tree))) 
-      (print-left (left-tree tree) (height (right-tree tree)) 0)
-      )))
- )
-
-
-;(trace visualize)
-;(trace print-left)
-;(trace print-right)
-;(trace get-n-chars)
-
-;дървото от условието
-;(10 (15 (2 () ()) (7 () ())) (5 (22 (2 () ()) (6 () ())) (1 () (3 (111 () ()) ()))))
-;
-;10-----5------1--3
-;|      |         |
-;|      |         111 
-;|      22--6
-;|      |
-;|      2
-;15--7
-;|
-;2
-;
-;      10
-;   /      \
-;  15       5  
-; / \      /  \  
-;2   7   22    1
-;       /  \    \
-;      2    6    3
-;               /
-;              111
-;
-;
-;BST: '(15 (5 (0 () ()) (10 () ())) (25 (20 () ()) (30 () ())))
-
-
+     (display
+      (string-append
+       (number->string (tree-root tree))
+       (print-right (right-tree tree) (width (left-tree tree)) (number-of-digits (tree-root tree))) 
+       (print-left (left-tree tree) (height (right-tree tree)) 0)
+       )
+      )
+     )
+    )
+  )
